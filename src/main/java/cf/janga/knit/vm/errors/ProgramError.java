3a937a04e1f298e397b4e5c933c36864b0180f0b
Emerson Loureiro <emerson.loureiro@gmail.com>
2022-10-05 22:38:26 +0100

    Some refactoring

--- src/main/java/cf/janga/knit/vm/errors/ProgramError.java
+++ src/main/java/cf/janga/knit/vm/errors/ProgramError.java
@@ -1,5 +1,10 @@
 package cf.janga.knit.vm.errors;
 
+/**
+ * Class of errors related to the semantic correctness of a
+ * program - such as use of undeclared variables, invalid
+ * variable assignments, etc.
+ */
 public class ProgramError extends RuntimeException {
 
     public ProgramError(String message) {
--- src/main/java/cf/janga/knit/vm/errors/RuntimeError.java
+++ src/main/java/cf/janga/knit/vm/errors/RuntimeError.java
@@ -1,20 +1,21 @@
 package cf.janga.knit.vm.errors;
 
+/**
+ * Class of errors related exclusively to a program's execution,
+ * as opposed to errors related to program semantics like using undeclared
+ * variables. Runtime errors are unexpected and unrecoverable - e.g.,
+ * division by 0, command failing to run.
+ */
 public class RuntimeError extends RuntimeException {
 
-    private final String _errorType;
+    private final String errorType;
 
     public RuntimeError(String message) {
         super(message);
-        _errorType = getClass().getName();
-    }
-
-    public RuntimeError(String message, String errorType) {
-        super(message);
-        _errorType = errorType;
+        this.errorType = getClass().getName();
     }
 
     public String getErrorType() {
-        return _errorType;
+        return this.errorType;
     }
 }
